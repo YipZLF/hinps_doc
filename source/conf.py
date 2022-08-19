5050f675b80958f2f450b6c50b0a754ef2761efa
Yipzlf <yipzlf@pku.edu.cn>
2022-08-19 14:52:18 +0800

    Add sphinx_rtd_theme

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser','sphinx_rtd_theme']
 myst_enable_extensions = [
     "amsmath",
     "colon_fence",
@@ -47,5 +47,5 @@ locale_dirs = ['../locales/']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
